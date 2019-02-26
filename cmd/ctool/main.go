e74e133e2c238b688b5aef632dce9f588ed8470f
guoguangkai <guoguangkai@wxblockchain.com>
2019-02-26 20:11:40 +0800

    first

---
+++ cmd/ctool/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+
+	"github.com/PlatONnetwork/PlatON-Go/cmd/ctool/core"
+	"github.com/PlatONnetwork/PlatON-Go/cmd/utils"
+	"gopkg.in/urfave/cli.v1"
+)
+
+var (
+	app = utils.NewApp("", "the wasm command line interface")
+)
+
+func init() {
+
+	// Initialize the CLI app
+	app.Commands = []cli.Command{
+		core.DeployCmd,
+		core.InvokeCmd,
+		core.CnsInvokeCmd,
+		core.CodeGenCmd,
+		core.SendTransactionCmd,
+		core.SendRawTransactionCmd,
+		core.GetTxReceiptCmd,
+		core.StabilityCmd,
+		core.StabPrepareCmd,
+	}
+	sort.Sort(cli.CommandsByName(app.Commands))
+	app.After = func(ctx *cli.Context) error {
+		return nil
+	}
+}
+
+func main() {
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
