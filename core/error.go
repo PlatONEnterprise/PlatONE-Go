1f9eea18a3d1d1a728ea53639b28e0e4f121d776
dumanxiang <dumanxiang@wxblockchain.com>
2019-03-05 17:27:27 +0800

    add cosume contract token as gas

--- core/error.go
+++ core/error.go
@@ -32,4 +32,6 @@ var (
 	// ErrNonceTooHigh is returned if the nonce of a transaction is higher than the
 	// next one expected based on the local chain.
 	ErrNonceTooHigh = errors.New("nonce too high")
+
+	ErrParamaManagerContractAddressNotFound  = errors.New("paramManager contract address not found")
 )
