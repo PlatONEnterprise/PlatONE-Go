f9d0b2e461df4279fd36cc1aeacc5c46d90e196c
gexin <gexin@wxblockchain.com>
2019-04-15 17:54:18 +0800

    comment some balance operations

--- core/state_transition.go
+++ core/state_transition.go
@@ -238,10 +238,10 @@ func (st *StateTransition) useGas(amount uint64) error {
 }
 
 func (st *StateTransition) buyGas() error {
-	mgval := new(big.Int).Mul(new(big.Int).SetUint64(st.msg.Gas()), st.gasPrice)
-	if st.state.GetBalance(st.msg.From()).Cmp(mgval) < 0 {
-		return errInsufficientBalanceForGas
-	}
+	//mgval := new(big.Int).Mul(new(big.Int).SetUint64(st.msg.Gas()), st.gasPrice)
+	//if st.state.GetBalance(st.msg.From()).Cmp(mgval) < 0 {
+	//	return errInsufficientBalanceForGas
+	//}
 	//if err := st.gp.SubGas(st.msg.Gas()); err != nil {
 	//	return err
 	//}
@@ -256,7 +256,7 @@ func (st *StateTransition) buyGas() error {
 	st.gas += gas
 
 	st.initialGas = gas
-	st.state.SubBalance(st.msg.From(), mgval)
+	//st.state.SubBalance(st.msg.From(), mgval)
 	return nil
 }
 
@@ -715,7 +715,7 @@ func (st *StateTransition) TransitionDb() (ret []byte, usedGas uint64, failed bo
 		vmerr = st.refundContractGas(feeContractAddr)
 	} else {
 		st.refundGas()
-		st.state.AddBalance(st.evm.Coinbase, new(big.Int).Mul(new(big.Int).SetUint64(st.gasUsed()), st.gasPrice))
+		//st.state.AddBalance(st.evm.Coinbase, new(big.Int).Mul(new(big.Int).SetUint64(st.gasUsed()), st.gasPrice))
 	}
 
 	return ret, st.gasUsed(), vmerr != nil, err
@@ -789,8 +789,8 @@ func (st *StateTransition) refundGas() {
 	st.gas += refund
 
 	// Return ETH for remaining gas, exchanged at the original rate.
-	remaining := new(big.Int).Mul(new(big.Int).SetUint64(st.gas), st.gasPrice)
-	st.state.AddBalance(st.msg.From(), remaining)
+	//remaining := new(big.Int).Mul(new(big.Int).SetUint64(st.gas), st.gasPrice)
+	//st.state.AddBalance(st.msg.From(), remaining)
 
 	// Also return remaining gas to the block gas counter so it is
 	// available for the next transaction.
