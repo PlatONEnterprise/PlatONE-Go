d6f73157d176fd8113c66f4cbce9d55c35f09160
tangyong <tangyong@wxblockchain.com>
2019-09-29 08:35:23 +0800

    add softfloat and del submodules

--- cmd/SysContracts/systemContract/nodeManager/nodeManager.cpp
+++ cmd/SysContracts/systemContract/nodeManager/nodeManager.cpp
@@ -539,3 +539,43 @@ BCWASM_ABI(systemContract::NodeManager, validJoinNode)
 BCWASM_ABI(systemContract::NodeManager, getNormalEnodeNodes)
 BCWASM_ABI(systemContract::NodeManager, getDeletedEnodeNodes)
 //bcwasm autogen begin
+extern "C" { 
+int add(const char * nodeJsonStr) {
+systemContract::NodeManager NodeManager_bcwasm;
+return NodeManager_bcwasm.add(nodeJsonStr);
+}
+const char * getAllNodes() {
+systemContract::NodeManager NodeManager_bcwasm;
+return NodeManager_bcwasm.getAllNodes();
+}
+int validJoinNode(const char * publicKey) {
+systemContract::NodeManager NodeManager_bcwasm;
+return NodeManager_bcwasm.validJoinNode(publicKey);
+}
+int nodesNum(const char * nodeJsonStr) {
+systemContract::NodeManager NodeManager_bcwasm;
+return NodeManager_bcwasm.nodesNum(nodeJsonStr);
+}
+const char * getNodes(const char * nodeJsonStr) {
+systemContract::NodeManager NodeManager_bcwasm;
+return NodeManager_bcwasm.getNodes(nodeJsonStr);
+}
+int update(const char * name,const char * nodeJsonStr) {
+systemContract::NodeManager NodeManager_bcwasm;
+return NodeManager_bcwasm.update(name,nodeJsonStr);
+}
+const char * getNormalEnodeNodes() {
+systemContract::NodeManager NodeManager_bcwasm;
+return NodeManager_bcwasm.getNormalEnodeNodes();
+}
+const char * getDeletedEnodeNodes() {
+systemContract::NodeManager NodeManager_bcwasm;
+return NodeManager_bcwasm.getDeletedEnodeNodes();
+}
+void init() {
+systemContract::NodeManager NodeManager_bcwasm;
+NodeManager_bcwasm.init();
+}
+
+}
+//bcwasm autogen end
