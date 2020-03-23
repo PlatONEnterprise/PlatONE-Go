c0724b95e2823127391dd4b30193ca4bed9dc75d
yuzongkun <yuzongkun@wxblockchain.com>
2020-03-23 17:19:05 +0800

    add unit test

--- life/contract/numberstest.cpp
+++ life/contract/numberstest.cpp
@@ -13,6 +13,14 @@ class Demo : public bcwasm::Contract{
     {
         bcwasm::println("init success...");
     }
+    int128_t addLong (int128_t i1,int128_t i2) const
+    {
+        return i1 + i2;
+    }
+    uint128_t addUlong (uint128_t i1,uint128_t i2) const
+    {
+        return i1 + i2;
+    }
     float  addFloat(float f1 ,float f2) const
     {
         return f1+f2;
@@ -29,6 +37,8 @@ class Demo : public bcwasm::Contract{
 };
 }
 BCWASM_ABI(demo::Demo, init)
+BCWASM_ABI(demo::Demo, addLong)
+BCWASM_ABI(demo::Demo, addUlong)
 BCWASM_ABI(demo::Demo, addFloat)
 BCWASM_ABI(demo::Demo, addDouble)
 BCWASM_ABI(demo::Demo, addLongDouble)
