6bb053355b8b0be6955c0fb5ab485c48941b7450
yuzongkun <yuzongkun>
2020-07-22 18:37:57 +0800

    floatToByte now using Big Endian,same as in ctool and wasm interpreter
    Former-commit-id: 620b9a7f4773e1ea05eeec7ea1e1729d6f19b6f1

--- common/bytes.go
+++ common/bytes.go
@@ -228,24 +228,24 @@ func BytesToInt64(b []byte) int64 {
 func Float32ToBytes(float float32) []byte {
 	bits := math.Float32bits(float)
 	bytes := make([]byte, 4)
-	binary.LittleEndian.PutUint32(bytes, bits)
+	binary.BigEndian.PutUint32(bytes, bits)
 	return bytes
 }
 
 func BytesToFloat32(bytes []byte) float32 {
-	bits := binary.LittleEndian.Uint32(bytes)
+	bits := binary.BigEndian.Uint32(bytes)
 	return math.Float32frombits(bits)
 }
 
 func Float64ToBytes(float float64) []byte {
 	bits := math.Float64bits(float)
 	bytes := make([]byte, 8)
-	binary.LittleEndian.PutUint64(bytes, bits)
+	binary.BigEndian.PutUint64(bytes, bits)
 	return bytes
 }
 
 func BytesToFloat64(bytes []byte) float64 {
-	bits := binary.LittleEndian.Uint64(bytes)
+	bits := binary.BigEndian.Uint64(bytes)
 	return math.Float64frombits(bits)
 }
 
