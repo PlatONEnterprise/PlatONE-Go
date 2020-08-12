80c719ee8e22d95f2a5e0bf1bdd538cf55c986d2
tofudfy <dengfuyang@outlook.com>
2020-08-12 10:30:43 +0800

    update the bug in fw and cns: 1. uniform cns name format check 2. complete fw rule format check

---
+++ core/vm/sc_firewall_test.go
@@ -0,0 +1,31 @@
+package vm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	fwTestAddr1 = "0x0000000000000000000000000000000000000123"
+	fwTestAddr2 = "0x0000000000000000000000000000000000000456"
+	fwTestErr   = "0x00000000000000000000000000000000000002"
+)
+
+func TestConvertToFwElem(t *testing.T) {
+	var err error
+	testCases := []struct {
+		rule     string
+		expected error
+	}{
+		{fwTestAddr1 + ":func1|" + fwTestAddr2 + ":func2", nil},
+		{fwTestAddr1 + "func1", ErrFwRule},
+		{fwTestErr + ":func1", ErrFwRuleAddr},
+	}
+
+	for _, data := range testCases {
+		_, err = convertToFwElem(data.rule)
+		assert.Equal(t, data.expected, err, "bug in convertToFwElem")
+	}
+
+}
